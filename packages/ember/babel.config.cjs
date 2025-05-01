cc5ab0212261d7457e382d91f2ac9b9c80934ff1
Max Babych <max22dev@gmail.com>
2025-05-01 17:01:08 +0300

    feat: init @livekit/components-ember

---
+++ packages/ember/babel.config.cjs
@@ -0,0 +1,32 @@
+const { buildMacros } = require('@embroider/macros/babel');
+
+const macros = buildMacros();
+
+module.exports = {
+  plugins: [
+    ['@babel/plugin-transform-typescript', {
+      allExtensions: true,
+      allowDeclareFields: true,
+      onlyRemoveTypeImports: true,
+    }],
+    [
+      'babel-plugin-ember-template-compilation',
+      {
+        transforms: [...macros.templateMacros],
+      },
+    ],
+    [
+      'module:decorator-transforms',
+      {
+        runtime: {
+          import: require.resolve('decorator-transforms/runtime-esm'),
+        },
+      },
+    ],
+    ...macros.babelMacros,
+  ],
+
+  generatorOpts: {
+    compact: false,
+  },
+};
