d36d72b8d6f93925721bcc5c6c58a6dcf9fd8ae0
Max Babych <max22dev@gmail.com>
2025-05-01 17:38:23 +0300

    feat: init test app

---
+++ examples/ember-test-app/app/app.ts
@@ -0,0 +1,19 @@
+import Application from '@ember/application';
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from "./config/environment";
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+
+import compatModules from "@embroider/virtual/compat-modules";
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
+
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver.withModules(compatModules);
+}
+
+loadInitializers(App, config.modulePrefix, compatModules);
---
+++ examples/ember-test-app/ember-cli-build.js
@@ -0,0 +1,17 @@
+'use strict';
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+const { compatBuild } = require('@embroider/compat');
+
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
+
+  const app = new EmberApp(defaults, {
+    'ember-cli-babel': { enableTypeScriptTransform: true },
+
+    // Add options here
+  });
+  return compatBuild(app, buildOnce, {
+    staticInvokables: true,
+  });
+};
